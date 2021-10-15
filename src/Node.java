df79d76b909085a2fff76954ba98e8a4b58fc8db
Nondas Ioannou <nevwalkalone13@gmail.com>
2021-10-15 11:54:47 +0300

    comments added in queue

--- src/Node.java
+++ src/Node.java
@@ -1,5 +1,12 @@
+/**
+ * Class representing a Node,
+ * contains an item and a reference
+ * to the next node.
+ * Generics were used.
+ */
 
 public class Node<T> {
+	
 	public T item;
 	public Node<T> next;
 
--- src/StringQueueImpl.java
+++ src/StringQueueImpl.java
@@ -1,7 +1,11 @@
 import java.io.PrintStream;
 import java.util.NoSuchElementException; 
 
-//H LEITOURGIA AUTHS THS KLASHS EPEKSHGEITAI ANALUTIKA STO PDF
+/**
+ * Class that implements the
+ * String Queue Interface's
+ * methods
+ */
 
 public class StringQueueImpl<T> implements StringQueue <T>{
 	
@@ -9,71 +13,108 @@ public class StringQueueImpl<T> implements StringQueue <T>{
 	private  int size=0;
 	private  Node<T> tail;
 	
+	// Default Constructor
+
 	public StringQueueImpl() {
-		
 	}
 	
+	// If there is no head
+	// this means the queue 
+	// is empty
+
 	public  boolean isEmpty() {
-		return head==null;
+		return head == null;
 	}
 	
+
 	public  void put(T item) {
-		
-		Node<T> node=new Node<>(item);
+
+        // creating a new node object
+		// that will be inserted
+
+		Node<T> node = new Node<>(item);
 		size++;
+
+		// if the queue is empty
+		// it will be the new head
+		// and tail simultaneously
+		// because it's the only element
+
 		if (isEmpty()) {
-			head=node;
-			
-		}
-		else {
-			
-			tail.next=node;
-			
-			
-			
+			head = node;
 		}
 
-		tail=node;
+		// else we connect the previous tail
+		// to this node, and make this node
+		// the tail
+
+		else {
+			tail.next = node;
+		}
+
+		tail = node;
 	}
 	
 	
 	public T get() throws NoSuchElementException {
+
+		// If queue is empty throw an Exception
+
 		if (isEmpty()) {
 			throw new NoSuchElementException("Queue is Empty");
 		}
-		
-		Node<T> oldhead=head;
-		
-		head=head.next;
-		oldhead.next=null;
+
+		// else remove the head and make
+		// the 2nd node as new head
+		// and return the old head
+
+		Node<T> oldhead = head;
+		head = head.next;
+		oldhead.next = null;
 		size--;
 		return oldhead.item;
-		}
+	}
 		
-	
-	
+	// returns head without removing it
+
 	public T peek()throws NoSuchElementException {
+
+		// if queue is empty throw an exception
+
 		if (isEmpty()) {
 			throw new NoSuchElementException("Queue is Empty");
 		}
+		// else return the head
+
 		return head.item;
 	}
-	
+
 	
 	public void printQueue(PrintStream stream) {
+
+		// if queue is empty
+		// print an appropriate
+		// message and return
+
 		if (isEmpty()) {
 			stream.println("Queue is empty");
 			return;
 		}
+
+		// else print each node
+
 		Node<T> node=head;
 		while(node!=null) {
 			stream.println(node.item);
-			node=node.next;
+			node = node.next;
 		}
 	}
 	
+	// returns size of queue
+
 	public int size() {
 		return size;
 	}
-	}
+
+}
 
