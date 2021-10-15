d6322d2086b8388e752a2060b9c875cb8d65c857
Nondas Ioannou <nevwalkalone13@gmail.com>
2021-10-15 13:17:38 +0300

    formatting of files

--- src/Node.java
+++ src/Node.java
@@ -1,17 +1,15 @@
 /**
- * Class representing a Node,
- * contains an item and a reference
- * to the next node.
+ * Class representing a Node, contains an item and a reference to the next node.
  * Generics were used.
  */
 
 public class Node<T> {
-	
+
 	public T item;
 	public Node<T> next;
 
 	public Node(T item) {
-		this.item=item;
-		this.next=null;
+		this.item = item;
+		this.next = null;
 	}
 }
--- src/StringQueue.java
+++ src/StringQueue.java
@@ -1,10 +1,9 @@
 import java.io.PrintStream;
-import java.util.NoSuchElementException; 
+import java.util.NoSuchElementException;
 
 /**
  * Defines the methods for a FIFO queue that handles String items
  */
-
 
 public interface StringQueue<T> {
     /**
@@ -18,35 +17,32 @@ public interface StringQueue<T> {
     public void put(T item);
 
     /**
-        * remove and return the oldest item of the queue
-        * @return oldest item of the queue
+     * remove and return the oldest item of the queue
+     * 
+     * @return oldest item of the queue
      * @throws NoSuchElementException if the queue is empty
      */
     public T get() throws NoSuchElementException;
 
     /**
      * return without removing the oldest item of the queue
+     * 
      * @return oldest item of the queue
      * @throws NoSuchElementException if the queue is empty
      */
     public T peek() throws NoSuchElementException;
 
-
     /**
-     * print the elements of the queue, starting from the oldest 
-        * item, to the print stream given as argument. For example, to 
-        * print the elements to the
-     * standard output, pass System.out as parameter. E.g.,
-     * printQueue(System.out);
+     * print the elements of the queue, starting from the oldest item, to the print
+     * stream given as argument. For example, to print the elements to the standard
+     * output, pass System.out as parameter. E.g., printQueue(System.out);
      */
     public void printQueue(PrintStream stream);
 
     /**
      * return the size of the queue, 0 if it is empty
+     * 
      * @return number of elements in the queue
      */
     public int size();
 }
-
-
-
