5c38ef753d87f163ba9156d3dc3753300312b6dd
Nondas Ioannou <nevwalkalone13@gmail.com>
2021-10-15 13:03:44 +0300

    Update StringQueue.java

--- src/StringQueue.java
+++ src/StringQueue.java
@@ -8,44 +8,44 @@ import java.util.NoSuchElementException;
 
 public interface StringQueue<T> {
     /**
-	 * @return true if the queue is empty
-	 */
-	public boolean isEmpty();
-
-	/**
-	 * insert a String item to the queue
-	 */
-	public void put(T item);
-
-	/**
-        * remove and return the oldest item of the queue
-        * @return oldest item of the queue
-	 * @throws NoSuchElementException if the queue is empty
-	 */
-	public T get() throws NoSuchElementException;
+     * @return true if the queue is empty
+     */
+    public boolean isEmpty();
 
     /**
-	 * return without removing the oldest item of the queue
-	 * @return oldest item of the queue
-	 * @throws NoSuchElementException if the queue is empty
-	 */
-	public T peek() throws NoSuchElementException;
+     * insert a String item to the queue
+     */
+    public void put(T item);
+
+    /**
+        * remove and return the oldest item of the queue
+        * @return oldest item of the queue
+     * @throws NoSuchElementException if the queue is empty
+     */
+    public T get() throws NoSuchElementException;
+
+    /**
+     * return without removing the oldest item of the queue
+     * @return oldest item of the queue
+     * @throws NoSuchElementException if the queue is empty
+     */
+    public T peek() throws NoSuchElementException;
 
 
-	/**
-	 * print the elements of the queue, starting from the oldest 
+    /**
+     * print the elements of the queue, starting from the oldest 
         * item, to the print stream given as argument. For example, to 
         * print the elements to the
-	 * standard output, pass System.out as parameter. E.g.,
-	 * printQueue(System.out);
-	 */
-	public void printQueue(PrintStream stream);
+     * standard output, pass System.out as parameter. E.g.,
+     * printQueue(System.out);
+     */
+    public void printQueue(PrintStream stream);
 
-	/**
-	 * return the size of the queue, 0 if it is empty
-	 * @return number of elements in the queue
-	 */
-	public int size();
+    /**
+     * return the size of the queue, 0 if it is empty
+     * @return number of elements in the queue
+     */
+    public int size();
 }
 
 
